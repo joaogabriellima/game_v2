7ec54effcdef913980eba5f113526bae0a7f1160
Joao Gabriel Lima <joaogabriel@gtacsolutions.com>
2020-03-05 16:53:10 -0300

    Sweet alert verification

--- server/public/scripts.js
+++ server/public/scripts.js
@@ -1,9 +1,46 @@
 
     
-$('.logout').on('click', function() {
-  window.location = 'login.html';
-});
+    $('.btnementa').on('click', function() {
+      $('#changeContent').load('ementa.html').hide().fadeIn(500);
+    });
 
-$('.loginbtn').on('click', function() {
-  window.location = '/index.html';
-});
+    $('.btnabout').on('click', function() {
+      $('#changeContent').load('about.html').hide().fadeIn(500);
+      $('#changePic').html('<img class="doctor-pic" src="img/drpedro_perfil.png">');
+    });
+
+
+    
+    $('.logout').on('click', function() {
+      window.location = 'login.html';
+    });
+    
+    $('.loginbtn').on('click', function(e) {
+      e.preventDefault();
+      var username = $('#username').val();
+      var password = $('#password').val();
+
+      if (username == '' || password == '') {
+        Swal.fire({
+          title: 'Atenção!',
+          text: 'Por favor preencha os campos corretamente!',
+          icon: 'warning',
+          confirmButtonText: 'Ok'
+        })
+        return;
+      }
+
+      if (username != 'admin' || password != 'admin') {
+        Swal.fire({
+          title: 'Erro!',
+          text: 'Não foi possível realizar login, cheque os dados e tente novamente',
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
+        return;
+      }
+
+
+
+      window.location.href = 'index.html';
+    });
